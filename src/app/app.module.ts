b233fd81a0148d22e793d812f8b45eb1c954ef47
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-20 10:32:24 -0500

    Añadida navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Core/header/header.component';
+import { MaterialModule } from './Shared/Modules/Material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
