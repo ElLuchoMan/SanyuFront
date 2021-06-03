fce75962c41549b62c6605b1d00329b739e17d44
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-03 12:31:26 -0500

    Home finalizado

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  cards = [
+    {
+      avatar: 'inventory_2',
+      title: 'Productos',
+      subtitle: 'Listado de nuestros productos',
+      img: 'https://i.imgur.com/tmDjT0m.jpg',
+      button: 'Ir',
+      urlTo: '/productos',
+    },
+    {
+      avatar: 'miscellaneous_services',
+      title: 'Servicios',
+      subtitle: 'Listado de nuestros servicios',
+      img: 'https://i.imgur.com/wfGbIhM.jpg',
+      button: 'Ir',
+      urlTo: '/servicios',
+    },
+    {
+      avatar: 'contact_mail',
+      title: 'Contacto',
+      subtitle: 'Contacta con nosotros',
+      img: 'https://i.imgur.com/nyWLQLy.jpg',
+      button: 'Ir',
+      urlTo: '/contacto',
+    },
+  ];
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Core/header/header.component';
 import { MaterialModule } from './Shared/Modules/Material/material.module';
@@ -23,6 +24,7 @@ import { DialogComponent } from './Shared/Components/dialog/dialog.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MaterialModule,
