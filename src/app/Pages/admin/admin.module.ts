4e199eb21ad94b5c1afc17454c96ca7ca16aac4f
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-23 09:40:48 -0500

    Se implementa toolbar en vista admin con 3 cards

--- src/app/Pages/admin/admin.module.ts
+++ src/app/Pages/admin/admin.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { HomeComponent } from './home/home.component';
+import { MaterialModule } from 'src/app/Shared/Modules/Material/material.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MaterialModule
   ]
 })
 export class AdminModule { }
--- src/app/Pages/admin/home/home.component.ts
+++ src/app/Pages/admin/home/home.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  cards = [
+    {
+      avatar: 'inventory_2',
+      title: 'Productos',
+      subtitle: 'Listado de nuestros productos',
+      img: 'https://i.imgur.com/tmDjT0m.jpg',
+      button: 'Productos',
+      urlTo: '/productos',
+    },
+    {
+      avatar: 'miscellaneous_services',
+      title: 'Servicios',
+      subtitle: 'Listado de nuestros servicios',
+      img: 'https://i.imgur.com/wfGbIhM.jpg',
+      button: 'Servicios',
+      urlTo: '/servicios',
+    },
+    {
+      avatar: 'contact_mail',
+      title: 'Contacto',
+      subtitle: 'Contacta con nosotros',
+      img: 'https://i.imgur.com/nyWLQLy.jpg',
+      button: 'Contacto',
+      urlTo: '/contacto',
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
